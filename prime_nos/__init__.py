22d97ee95e2e6763568ea2a20023b3cc8378e69a
Vera <vera20ec@cmrit.ac.in>
2025-05-09 13:45:34 +0530

    fixed everything

--- prime_nos/__init__.py
+++ prime_nos/__init__.py
@@ -1,3 +1,3 @@
 from .core import is_prime, are_coprime
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prime_nos",
-    version="0.1.2",
+    version="0.2.0",
     author="Venkatesh",
     description="A simple library to check for prime and coprime numbers",
     long_description=open("README.md").read(),
