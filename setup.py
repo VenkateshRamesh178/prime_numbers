43496655a6143907e6fec1a3293589ba49fdcaf8
Vera <vera20ec@cmrit.ac.in>
2025-05-13 16:24:15 +0530

    🔖 Release version 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prime_nos",
-    version="0.2.4",
+    version="0.2.5",
     author="Venkatesh",
     description="A simple library to check for prime and coprime numbers",
     long_description=open("README.md").read(),
